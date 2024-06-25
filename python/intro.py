eb549d95f40079b5c6832634aba8c37585dfa8ac
isabel <isabeltak1228@gmail.com>
2024-06-24 20:09:24 -0400

    Add files via upload

---
+++ python/intro.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+month = input("What month were you born in? ")
+
+int (age)
+
+print("Hi", name, "nice to meet you! You are", age, "years old. Born in", month)
